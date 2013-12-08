eb2a64836d2d2805ea74d9e418989313e99c9cea
Graeme Lockwood <graeme.lockwood@gmail.com>
2013-12-08 10:58:58 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clydebuilt::Application.config.secret_key_base = 'd9dbea66319a8517e25e2fe2bd8a480dddd550a51abbe0ddff1a13444d02077eb8fd575edf4bae699d6ab769c0151304a864c3a3c9d9e0ae5325b44a60b4b6d7'
